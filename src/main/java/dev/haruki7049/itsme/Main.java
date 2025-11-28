28f0258cf8c1eea681ef40f886387f62d8b77551
haruki7049 <tontonkirikiri@gmail.com>
2025-11-28 18:38:17 +0900

    Update pom.xml

--- src/main/java/dev/haruki7049/itsme/Main.java
+++
@@ -1,8 +0,0 @@
-package dev.haruki7049.itsme;
-
-/** Main class for the itsme parser combinator library. */
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("itsme - A parser combinator library for Java");
-  }
-}
