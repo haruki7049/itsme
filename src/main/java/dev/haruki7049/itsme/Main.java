f2fb44fca85398901bfe6a949121165a4ca46762
haruki7049 <tontonkirikiri@gmail.com>
2025-11-28 18:16:15 +0900

    style: nix fmt

--- src/main/java/dev/haruki7049/itsme/Main.java
+++ src/main/java/dev/haruki7049/itsme/Main.java
@@ -1,10 +1,8 @@
 package dev.haruki7049.itsme;
 
-/**
- * Main class for the itsme parser combinator library.
- */
+/** Main class for the itsme parser combinator library. */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("itsme - A parser combinator library for Java");
-    }
+  public static void main(String[] args) {
+    System.out.println("itsme - A parser combinator library for Java");
+  }
 }
